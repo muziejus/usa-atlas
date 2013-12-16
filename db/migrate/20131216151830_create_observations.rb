1b8c07a423b85ea5b79c22baabea92c9e1c09faa
Moacir P. de Sá Pereira <moacir@gmail.com>
2013-12-16 18:05:11 +0200

    Add minimal Observation model

--- db/migrate/20131216151830_create_observations.rb
+++ db/migrate/20131216151830_create_observations.rb
@@ -1,7 +1,9 @@
 class CreateObservations < ActiveRecord::Migration
   def change
     create_table :observations do |t|
-
+      t.text "text"
+      t.string "place"
+      t.boolean "dialogue", default: false
       t.timestamps
     end
   end
