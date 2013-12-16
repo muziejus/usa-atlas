8c047a7f02e7294f724c61684c8d9bedbfc042bc
Moacir P. de Sá Pereira <moacir@gmail.com>
2013-12-16 16:09:20 +0200

    Initial Rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UsaAtlas::Application.config.secret_key_base = 'dfa40329ba8df598f9a2e2e396cd3540a823f2fab63fd60a9f06f60ba2d342abc4032304a1cfeb3915facced7858e590b2b7df3ae2f7ed51f83b5022f32b9550'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UsaAtlas::Application.config.session_store :cookie_store, key: '_usa-atlas_session'
