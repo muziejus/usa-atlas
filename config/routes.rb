9e13da52471cc929d07ae52bdcc8bef081e19579
Moacir P. de Sá Pereira <moacir@gmail.com>
2013-12-16 18:14:56 +0200

    Add Observation resources route

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ UsaAtlas::Application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
 
+  resources :observations
+
   # Example resource route with options:
   #   resources :products do
   #     member do
